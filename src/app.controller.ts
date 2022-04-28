d4aaea9b6ce5fdcb7c2525cff1867ef26548ac98
Alwin Schuster <alwin.schuster@chello.at>
2022-04-28 17:26:56 +0200

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TestClass2 } from './classes';
+import { plainToClass } from 'class-transformer';
 
 @Controller()
 export class AppController {
@@ -17,6 +18,14 @@ export class AppController {
   getHello(@Body() data: TestClass2): string {
     console.log("Expected: TestClass2 { myArray: [ { foo: 'hi' } ] }");
     console.log('Actual:  ', data);
+    console.log(
+      'Actual 2:  ',
+      plainToClass(
+        TestClass2,
+        { myArray: [{ foo: 'hi' }] },
+        { enableImplicitConversion: true },
+      ),
+    );
     return this.appService.getHello();
   }
 }
